18009f556e8b325186a3e480069ed4e25e04083d
Unknown <marreg@rocketmail.com>
2017-11-16 14:35:04 +0100

    Ajout jquery Ajout quelques fonctions Etat: Meh

--- indexu.php
+++ indexu.php
@@ -2,25 +2,55 @@
 <html>
 <head>
 	<title>Formulaire équipe</title>
+	<script src="jquery-3.2.1.min.js"></script>
 </head>
 <body>
 <ul id="Formulaire">
-	
+
 </ul>
 <p id="boutonOk">
-	<button onclick="creationForm();"> Creation du Formulaire </button>
+	<input type="text" id="question">
+	<button onclick="creationForm();" id="boutonDuFormulaire"> Creation du Formulaire </button>
 </p>
 
 <script>
+	function creationQuestion(){
+		console.log(document.getElementById("question").value);
+		document.getElementById("Formulaire").innerHTML += document.getElementById("question").value+'<br>';
+	}
+
 	function creationForm(){
-		console.log('coucou');
-		
-		document.getElementById('boutonOk').innerHTML += "<button onclick='ajoutTextForm();''> ajout d'un champ </button> <button onclick='nouveauForm();'> Nouveau formulaire </button>";
+		console.log('hello');
+		creationQuestion();
+		ajoutTextForm();
+		deleteOk();
+	}
+	function deleteLi() {
+		$('#unique').remove();
+	}
+	function initiationRedemarage(){
+		console.log('test');
+	}
+	//Supprime de quoi créer formulaire
+	function deleteOk(){
+		document.getElementById('boutonOk').innerHTML = '<button id="boutonUnique" onclick="modOk();">ok</button> <button onclick="initiationRedemarage();">Fin</button>';
+	}
+	//<button id='boutonUnique' onclick='modOk();'>ok</button>
+	//Fonction pour ajouter du texte dans le ul
+	function ajoutTextForm(){
+		document.getElementById("Formulaire").innerHTML += "<li id='unique'><input type='text' id='monText'></li>";
+		//document.getElementById('boutonOk').innerHTML = '';
+	}
+	function modOk(){
+		document.getElementById('Formulaire').innerHTML += "<input type='radio'><li>"+document.getElementById('monText').value+'</li> <br>';
+		deleteLi();
+		//document.getElementById().innerHTML = "";
 		ajoutTextForm();
 	}
-	function ajoutTextForm(){
-		document.getElementById("Formulaire").innerHTML += "<li><input type='text'></li>"; 
-	}
+	/*function simplification(){
+		document.getElementById('boutonOk').innerHTML += "<button onclick='ajoutTextForm();''> ajout d'un champ </button> <button onclick='nouveauForm();'> Nouveau formulaire </button>";
+		ajoutTextForm();
+	}*/
 </script>
 
 </body>
