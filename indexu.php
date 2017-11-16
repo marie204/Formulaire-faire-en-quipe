0b7d97cf36338ad0ad2f2e765779b60e16ab21d0
Unknown <marreg@rocketmail.com>
2017-11-16 16:23:28 +0100

    Réglage des soucis de boutons pas tip top

--- indexu.php
+++ indexu.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
 	<title>Formulaire équipe</title>
 	<script src="jquery-3.2.1.min.js"></script>
 </head>
@@ -20,7 +21,7 @@
 		document.getElementById('Formulaire').innerHTML += document.getElementById("question").value+'<br>';
 	}
 	function gestionnaireDuBoutonOk(){
-		document.getElementById('boutonOk').innerHTML = '<input type="text" id="question"><button onclick="creationForm();" id="boutonDuFormulaire"> Creation du Formulaire </button>';
+		document.getElementById('boutonOk').innerHTML = '<input type="text" id="question"><button onclick="creationForm();" id="boutonDuFormulaire"> Creation d\'une question </button>';
 	}
 	function createPara(){
 		deleteLi();
@@ -54,7 +55,7 @@
 		//document.getElementById('boutonOk').innerHTML = '';
 	}
 	function modOk(){
-		document.getElementById('Formulaire').innerHTML += "<input type='radio' name= 'A"+paraUl+"'><li>"+document.getElementById('monText').value+'</li> <br>';
+		document.getElementById('Formulaire').innerHTML += "<li> <input type='radio' name ='A"+paraUl+"'>"+document.getElementById('monText').value+'</li> <br>';
 		deleteLi();
 		//document.getElementById().innerHTML = "";
 		ajoutTextForm();
